05b0f1af85715025d3ba680d3959375549ca65c0
nathan <nlai274@gmail.com>
2024-01-12 01:05:57 -0800

    storing data on memory to reduce database calls

--- api_modules/tft/api.js
+++ api_modules/tft/api.js
@@ -3,6 +3,8 @@ const {getOracleCredentials} = require('../../utils/oracle')
 const oracledb = require('oracledb');
 
 const ROUTE = 'tft';
+var poolSizes = {};
+var champions = {};
 
 function loadModule(expressApp){
     createGet(":set/:champion/3star", expressApp, async (req, res) => {
@@ -16,53 +18,61 @@ function loadModule(expressApp){
         var chanceOfFreeRoll = req.query.chanceOfFreeRoll || "";
         var championDuplicators = req.query.championDuplicators || "";
 
-        const oracleLogin = getOracleCredentials();
-        const connection = await oracledb.getConnection(oracleLogin);
-        var result = null;
-        try{
-            var poolSizes = {};
-            var champion = {name: championName, traits: []};
+        // checking if the needed data is stored in memory
+        let hasPoolSize = Object.keys(poolSizes).length > 0;
+        let hasChampion = Object.keys(champions).includes(championName);
 
-            // getting pool sizes
-            let sqlString = `
-            SELECT cost, pool_size
-            FROM pool_sizes
-            WHERE set_number=:setNumber
-            `;
-            result = await connection.execute(sqlString, {setNumber: setNumber}, {});
-            if(result.rows.length > 0){
-                for(let poolSize of result.rows){
-                    poolSizes[poolSize[0]] = poolSize[1];
+        // getting champion and pool size data from the database
+        if(!hasPoolSize || !hasChampion){
+            const oracleLogin = getOracleCredentials();
+            const connection = await oracledb.getConnection(oracleLogin);
+            var result = null;
+            try{ 
+                // getting pool sizes
+                if(!hasPoolSize){
+                    let sqlString = `
+                    SELECT cost, pool_size
+                    FROM pool_sizes
+                    WHERE set_number=:setNumber
+                    `;
+                    result = await connection.execute(sqlString, {setNumber: setNumber}, {});
+                    if(result.rows.length > 0){
+                        for(let poolSize of result.rows){
+                            poolSizes[poolSize[0]] = poolSize[1];
+                        }
+                    }
+                    else
+                        return res.status(404).send({message: 'Set not found!'});
+                }
+                // champion data
+                if(!hasChampion){
+                    let sqlString2 = `
+                    SELECT cost, trait_name
+                    FROM champion_traits INNER JOIN champions 
+                    USING (set_number, champion_name)
+                    WHERE set_number=:setNumber AND champion_name=:championName 
+                    `;
+                    result = await connection.execute(sqlString2, {setNumber: setNumber, championName: championName}, {});
+                    if(result.rows.length > 0){
+                        champions[championName] = {traits: []};
+                        champions[championName].cost = result.rows[0][0];
+                        for(let championTrait of result.rows){
+                            champions[championName].traits.push(championTrait[1]);
+                        }
+                    }
+                    else
+                        return res.status(404).send({message: 'Champion not found!'});
                 }
             }
-            else
-            return res.status(404).send({message: 'Set not found!'});
-
-
-            // champion data
-            let sqlString2 = `
-            SELECT cost, trait_name
-            FROM champion_traits INNER JOIN champions 
-            USING (set_number, champion_name)
-            WHERE set_number=:setNumber AND champion_name=:championName 
-            `;
-            result = await connection.execute(sqlString2, {setNumber: setNumber, championName: championName}, {});
-            if(result.rows.length > 0){
-                champion.cost = result.rows[0][0];
-                for(let championTrait of result.rows){
-                    champion.traits.push(championTrait[1]);
-                }
+            catch(error){
+                console.log(error);
             }
-            else
-                return res.status(404).send({message: 'Champion not found!'});
+            finally{
+                connection.close();
+            }
+    
         }
-        catch(error){
-            console.log(error);
-        }
-        finally{
-            connection.close();
-        }
-
+        
         res.send(`received ${req.params.set} and ${req.params.champion}, optional: ${championsHeld} ${championsAcquired} ${chanceOfFreeRoll} ${championDuplicators}`);
 
     });
